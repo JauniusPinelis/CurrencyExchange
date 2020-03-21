8b5234666215506ee50a027ed4f9db4d0839000b
JauniusPinelis <JauniusPinelis@gmail.com>
2020-03-21 23:43:07 +0200

    Writing first test for Calculator.cs

---
+++ Tests/CurrencyExchange.Tests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using CurrencyExchange.Domain.Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyExchange.Domain.Tests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator;
+        public CalculatorTests()
+        {
+            var exchangeRates = TestData.GetExchangeRates();
+            _calculator = new Calculator(exchangeRates);
+        }
+
+        public void Calculate_GivenEurDKK1_ReturnsCorrectResult()
+        {
+            var conversion = new Conversion()
+            {
+                from= new 
+            }
+        }
+    }
+}
---
+++ src/CurrencyExchange.Domain/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using CurrencyExchange.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyExchange.Domain
+{
+    public class Calculator
+    {
+        private readonly IEnumerable<Currency> _exchangeRates;
+
+        public Calculator(IEnumerable<Currency> exchangeRates)
+        {
+            _exchangeRates = exchangeRates;
+        }
+
+        public decimal Calculate(Conversion conversion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
